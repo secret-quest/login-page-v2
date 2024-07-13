112a28b6f001c57917d22fbae0ade1e71531fd48
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-13 13:40:10 +0200

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,16 @@
 import Header from "./header"
-import Footer from "./footer"
-import type { ReactNode } from "react"
+import { ReactNode } from "react"
+import styles from "./layout.module.css"
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className={styles.contentWrapper}>
       <Header />
       <main>{children}</main>
-      <Footer />
-    </>
+      <video autoPlay muted loop playsInline id="bg-video" className={styles.bgVideo}>
+        <source src="/v7.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   )
 }
