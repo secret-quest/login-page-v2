b16d25ed6b5d54f7af8040606337aa4cd2d575ec
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-13 16:32:05 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@ import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 export default function Header() {
   return (
     <header>
+      <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet"></link>
       <div className="container">
         <nav>
           <div className="logo">Secret Quest</div>
