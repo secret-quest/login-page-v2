48a86988eed5f55ef67c4f3278c46c3e993b7d85
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-13 13:45:53 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,12 @@
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
-import Link from "next/link";
-import styles from "./header.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <nav className={styles.nav}>
-          <div className={styles.logo}>Secret Quest</div>
-          <div className={styles.navLinks}>
+    <header>
+      <div className="container">
+        <nav>
+          <div className="logo">Secret Quest</div>
+          <div className="nav-links">
             <DynamicWidget />
           </div>
         </nav>
