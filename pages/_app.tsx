e3ef88fee15e68460c9e80e23d29c6ceef4512f3
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-13 13:40:18 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,25 @@
 import { SessionProvider } from "next-auth/react"
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import "./styles.css"
 
 import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
 
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object.
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <DynamicContextProvider
+        settings={{
+          environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID!,
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <Component {...pageProps} />
+      </DynamicContextProvider>
     </SessionProvider>
   )
 }
